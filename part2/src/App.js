e3760312bee8415eeabcf492fdeb7293f7c28466
juan carrero <juancarrero811@gmail.com>
2021-02-21 17:44:40 -0430

    2.7: The Phonebook Step2

--- part2/src/App.js
+++ part2/src/App.js
@@ -8,6 +8,12 @@ const App = () => {
 
 	const handleSubmit = (event) => {
 		event.preventDefault()
+		if (persons.some(item => item.name === newName)) {
+			alert(`${newName} is already added to phonebook`)
+			setNewName('')
+			return
+		}
+
 		setPersons([...persons, { name: newName }])
 		setNewName('')
 	}
