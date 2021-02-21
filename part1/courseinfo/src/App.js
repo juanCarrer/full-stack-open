3db3f1dcff296847b14c7ac427aa3851e4f6cace
juan carrero <juancarrero811@gmail.com>
2021-02-21 01:15:38 -0430

    1.13*: anecdotes step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,17 +1,26 @@
 import React, { useState } from 'react'
 
 export const App = ({ anecdotes = [] }) => {
-  const [selected, setSelected] = useState(0)
+	const empyVotes = Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0)
+	const [selected, setSelected] = useState(0)
+	const [votes, setVotes] = useState(empyVotes)
+	
+	const handleAnecdote = () => {
+		setSelected(Math.floor(Math.random() * anecdotes.length))
+	}
 
-  const handelClick = () => {
-  	setSelected(Math.floor(Math.random() * anecdotes.length))
-  }
+	const handleVotes = () => {
+		const newState = [...votes]
+		newState[selected]++
+		setVotes(newState)
+	}
 
-  return (
-    <div>
-      {anecdotes[selected]}
-      <br/>
-    	<button onClick={handelClick}>next anecdote</button>
-    </div>
-  )
+	return (
+		<div>
+			<p>{anecdotes[selected]}</p>
+			<p>has {votes[selected]} votes</p>
+			<button onClick={handleVotes}>vote</button>
+			<button onClick={handleAnecdote}>next anecdote</button>
+		</div>
+	)
 }
