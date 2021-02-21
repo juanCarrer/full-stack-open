c4ff842ba99f10c57e565a17dc0721de0713d4a8
juan carrero <juancarrero811@gmail.com>
2021-02-21 02:05:27 -0430

    1.14*: anecdotes step3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,15 @@
 import React, { useState } from 'react'
 
+
+const Anecdote = ({ anecdotes, votes, index }) => {
+	return (
+		<div>
+			<p>{anecdotes[index]}</p>
+			<p>has {votes[index]} votes</p>
+		</div>
+	)
+}
+
 export const App = ({ anecdotes = [] }) => {
 	const empyVotes = Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0)
 	const [selected, setSelected] = useState(0)
@@ -15,12 +25,16 @@ export const App = ({ anecdotes = [] }) => {
 		setVotes(newState)
 	}
 
+	const mostVotedIndex = votes.indexOf(Math.max(...votes))
+
 	return (
 		<div>
-			<p>{anecdotes[selected]}</p>
-			<p>has {votes[selected]} votes</p>
+			<h1>Anecdote of the day</h1>
+			<Anecdote anecdotes={anecdotes} votes={votes} index={selected}/>
 			<button onClick={handleVotes}>vote</button>
 			<button onClick={handleAnecdote}>next anecdote</button>
+			<h1>Anecdote with most votes</h1>
+			<Anecdote anecdotes={anecdotes} votes={votes} index={mostVotedIndex}/>
 		</div>
 	)
 }
