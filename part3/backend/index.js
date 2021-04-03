a96bb007fd72fdf3af2446ad06ba295add8323d2
juan carrero <juancarrero811@gmail.com>
2021-04-02 19:48:44 -0700

    3.13: Phonebook database, step1. adding files

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/Person')
 
 const app = express()
-	console.log('visto desde terminal', {bj: "este es un texto desde termisnsla"})
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
@@ -17,50 +18,42 @@ morgan.token('body', (request) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-	{
-		id: 1,
-		name: 'Arto Hellas',
-		number: '040-123456',
-	},
-	{
-		id: 2,
-		name: 'Ada Lovelace',
-		number: '39-44-5323523',
-	},
-	{
-		id: 3,
-		name: 'Dan Abramov',
-		number: '12-43-234345',
-	},
-	{
-		id: 4,
-		name: 'Maty Poppendick',
-		number: '39-23-6423122',
-	}
-]
 
 app.get('/api/persons', (request, response) => {
-	response.json(persons)
+	Person.find({})
+		.then(data => {
+			response.json(data)
+		})
+		.catch(error => {
+			console.error('error on fetch data', error)
+			response.json({ Error: error.message })
+		})
 })
 
 app.get('/info', (request, response) => {
-	response.send(`
-		<p>Phonebook has info for ${persons.length} persons</p>
-		<p>${new Date()}<p>
-	`)
+	Person.find({})
+		.then(data => {
+			response.send(`
+				<p>Phonebook has info for ${data.length} persons</p>
+				<p>${new Date()}<p>
+			`)
+		})
+		.catch(error => {
+			console.log(error.message)
+			response.status(500).end()
+		})
 })
 
 app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
 
-	const person = persons.find(item => item.id === id)
-
-	if (person) {
-		response.json(person)
-	} else {
-		response.status(404).end()
-	}
+	Person.findById(request.params.id)
+		.then(data => {
+			response.json(data)
+		})
+		.catch(error => {
+			console.error(error)
+			response.status(404).end()
+		})
 })
 
 app.delete('/api/persons/:id', (request, response) => {
---
+++ part3/backend/models/Person.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const url = process.env.mongoURL
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    console.log('[DB] connected')
+  })
+  .catch((e) => {
+    console.log('Error Conecting to DB', e)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports =  mongoose.model('Person', personSchema)
+
+
