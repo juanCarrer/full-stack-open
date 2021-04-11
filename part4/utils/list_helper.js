091267d8f4d096996d574d19a0bb7e75fdbf4e2e
juan carrero <juancarrero811@gmail.com>
2021-04-11 09:54:14 -0700

    4.4: helper functions and unit tests, step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likes = blogs.reduce((accumulator, curentValue) => {
+    return { likes: accumulator.likes + curentValue.likes }
+  })
+  return likes.likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
